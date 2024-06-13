c22789a5fbfdccd73a9b70ff90050bceb6ed8325
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-06-13 13:34:47 +0700

    Buat right child dari node baru ke null

--- BinarySearchTree__lia.cpp
+++ BinarySearchTree__lia.cpp
@@ -35,6 +35,7 @@ public:
         Node* newNode = new Node(element, nullptr, nullptr);
         newNode->info = element;
         newNode->leftchild = nullptr;
+        newNode->rightchild = nullptr;
 
     }
 
