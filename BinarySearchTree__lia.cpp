62eaa7e34758d924c2519f04d5f499eb2b9c695c
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-06-13 13:44:59 +0700

    Deklarasi void preorder include cs if didalamnya

--- BinarySearchTree__lia.cpp
+++ BinarySearchTree__lia.cpp
@@ -86,6 +86,22 @@ public:
         }
     }
 
+    void preorder(Node* ptr)
+    {
+        if (ROOT == nullptr)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr != nullptr)
+        {
+            cout << ptr->info << " ";
+            preorder(ptr->leftchild);
+            preorder(ptr->rightchild);
+
+        }
+    }
+
 };
 
 
