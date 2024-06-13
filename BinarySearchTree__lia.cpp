9843c0414ef80227eb42b13b6d0102b6538bffe3
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-06-13 13:21:00 +0700

    Add project files.

---
+++ BinarySearchTree__lia.cpp
@@ -0,0 +1,12 @@
+// BinarySearchTree__lia.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
